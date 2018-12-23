d0c8c06717d713e23163efa0457df152ffa47b1e
Viktor Braun (v_b) <v-braun@live.de>
2018-12-23 18:00:49 +0100

    - restructured xcode project

---
+++ VBRProceduralStrings/VBRProceduralStrings/VBRProceduralStrings.swift
@@ -0,0 +1,11 @@
+//
+//  VBRProceduralStrings.swift
+//  VBRProceduralStrings
+//
+//  Created by Viktor Braun on 23.12.2018.
+//  Copyright © 2018 Viktor Braun - Software Development. All rights reserved.
+//
+
+class VBRProceduralStrings {
+
+}
